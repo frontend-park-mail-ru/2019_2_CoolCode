4b946c287bb6347c9684386b40c63eee61de1e5f
AntonPriyma <apriyma0@gmail.com>
2019-09-29 22:15:27 +0300

    [FIX]

--- public/modules/API/profile.js
+++ public/modules/API/profile.js
@@ -64,11 +64,12 @@ function getUserPhoto (id) {
         mode: 'cors',
     }).then(response => {
         response.arrayBuffer().then((buffer) => {
+            profile.hideLoader();
             if (response.status !== 200) {
                 throw new Error(
                     `Не зашли: ${response.status}`);
             }
-            profile.hideLoader();
+
             let base64Flag = 'data:image/jpeg;base64,';
             let imageStr = arrayBufferToBase64(buffer);
 
