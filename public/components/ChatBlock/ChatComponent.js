2defe540250c7e97d55cb3d2c8c0e835713d6295
Nastya <kot.siniy@mail.ru>
2019-12-10 01:24:19 +0300

    [add] audioMessage_new

--- public/components/ChatBlock/ChatComponent.js
+++ public/components/ChatBlock/ChatComponent.js
@@ -45,6 +45,13 @@ class ChatComponent extends BaseComponent {
     	contentListRoot.scrollTop = contentListRoot.scrollHeight - contentListRoot.clientHeight;
     }
 
+    renderOutgoingRecord(messageData) {
+    	const contentListRoot = this._parent.querySelector(this.contentListRootSelector);
+    	const messageComponent = new ChatMessageComponent({message: messageData, user: this._data.user, error: false, deleted:false, edited:false}, contentListRoot);
+    	contentListRoot.appendChild(messageComponent.render());
+    	contentListRoot.scrollTop = contentListRoot.scrollHeight - contentListRoot.clientHeight;
+    }
+
     renderCurrentChatIncomingMessage(messageData) {
     	const contentListRoot = this._parent.querySelector(this.contentListRootSelector);
     	const messageComponent = new ChatMessageComponent({message: messageData, user: this._data.user, error: false, deleted:false, edited:false}, contentListRoot);
